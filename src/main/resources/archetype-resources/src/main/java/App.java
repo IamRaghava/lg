6b40c8ed47bc7fbc55a748d75596701611532959
IamRaghava <137440935+IamRaghava@users.noreply.github.com>
2024-02-20 20:38:00 +0530

    Update App.java

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello    World   good morning!" );
+        System.out.println( "Hello  World   good morning!" );
     }
 }
 public class PrimeExample{    
